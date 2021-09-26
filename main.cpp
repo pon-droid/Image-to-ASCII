4499644aec2442810e9505f73eaf196fae3586d7
tbg <tellepsarecool@gmail.com>
2021-09-26 20:27:57 +1000

    Added inferring RGB values from image input

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <Magick++.h>
 #include <iostream>
+#include <vector>
 using namespace std;
 using namespace Magick;
 int main(int argc,char **argv)
@@ -12,23 +13,34 @@ int main(int argc,char **argv)
   Image image;
   try {
     // Read a file into image object
-    image.read( "girl.gif" );
+    image.read( "ascii-pineapple.jpg" );
+
+    cout << "Image Loaded" << endl;
+
+    //Get width and height, get maximum RGB value
+    const int IMAGE_W = image.rows();
+    const int IMAGE_H = image.columns();
+    const int MAX_COLOUR = 255;
+
+    cout << "Image size : " << IMAGE_W << " x " << IMAGE_H << endl;
+    //Fetches colour values of each pixel and prints them
+    for(int y = 0; y<IMAGE_H; y++){
+        for(int x = 0; x<IMAGE_W; x++){
+            ColorRGB colour = image.pixelColor(x, y);
+
+            double red = colour.red() * MAX_COLOUR;
+            double blue = colour.blue() * MAX_COLOUR;
+            double green = colour.green() * MAX_COLOUR;
 
 
 
-    cout << image.columns() << endl;
-    cout << image.rows() << endl;
+            cout << " Red is : " << red << " Blue is : " << blue << " Green is : " << green << endl;
 
-    // Crop the image to specified size (width, height, xOffset, yOffset)
-    image.crop( Geometry(100,100, 300, 300) );
 
-    cout << image.columns() << endl;
-    cout << image.rows() << endl;
+        }
+    }
 
-    // Write the image to a file
-    image.write( "x.gif" );
 
-    image.display();
   }
   catch( Exception &error_ )
     {
