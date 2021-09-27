43b8c109702aa99a19c915458f8d20d084d21dd3
tbg <tellepsarecool@gmail.com>
2021-09-27 10:04:09 +1000

    Basic ASCII character mapping

--- main.cpp
+++ main.cpp
@@ -20,24 +20,47 @@ int main(int argc,char **argv)
     //Get width and height, get maximum RGB value
     const int IMAGE_W = image.rows();
     const int IMAGE_H = image.columns();
-    const int MAX_COLOUR = 255;
+    const unsigned char MAX_COLOUR = 255;
 
     cout << "Image size : " << IMAGE_W << " x " << IMAGE_H << endl;
     //Fetches colour values of each pixel and prints them
-    for(int y = 0; y<IMAGE_H; y++){
-        for(int x = 0; x<IMAGE_W; x++){
+    cout << "Iterating through brightness" << endl;
+    for(int y = 0; y<IMAGE_W; y++){
+        for(int x = 0; x<IMAGE_H; x++){
             ColorRGB colour = image.pixelColor(x, y);
+             // Proper RGB values
+             unsigned char red = colour.red() * MAX_COLOUR;
+             unsigned char green = colour.green() * MAX_COLOUR;
+             unsigned char blue = colour.blue() * MAX_COLOUR;
+                // Brightness algorithm and scaling to ASCII mapping
+             unsigned char brightness = (red + green + blue)/3;
+             unsigned char ascii_scaling = brightness/3;
 
-            double red = colour.red() * MAX_COLOUR;
-            double blue = colour.blue() * MAX_COLOUR;
-            double green = colour.green() * MAX_COLOUR;
+             //ASCII printing
+             switch(ascii_scaling){
+             case 0 ... 20:
+             cout << '`';
+             break;
+             case 21 ... 40:
+             cout << '^';
+             break;
+             case 41 ... 60:
+             cout << '/';
+             break;
+             case 61 ... 80:
+             cout << ",";
+             break;
 
 
+             default:
+                 cout << ':';
 
-            cout << " Red is : " << red << " Blue is : " << blue << " Green is : " << green << endl;
+             }
 
 
         }
+        //New line each y change
+             cout << endl;
     }
 
 
